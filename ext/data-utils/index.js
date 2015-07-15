03edae467e3d9198b928e28284ca2cce6f6f49b4
Charles Scalfani <charles@panosoft.com>
2015-07-15 09:52:19 -0700

    Seeded with some utils

---
+++ ext/data-utils/index.js
@@ -0,0 +1,26 @@
+var R = require('ramda');
+
+module.exports = {
+	callOn: R.curry((obj, f) => f(obj)),
+	sumProps: R.curry((props, obj) => {
+		return R.sum(R.map(R.callOn(obj), R.map(R.prop, props)));
+	}),
+	sumColumn: R.curry((prop, objs) => {
+		return R.sum(R.map(R.prop(prop), objs));
+	}),
+	compareProps: R.curry(function(props, a, b) {
+		// determine property compare function (lt or gt) based on + or -
+		var propCompares = R.map(prop => prop[0] == '-' ? R.gt : R.lt, props);
+		// remove + and - from property names
+		props = R.map(R.replace(/^(-|\+)/, ''), props);
+		// determine which properties are equal
+		var equalProps = R.map(prop => R.equals(a[prop], b[prop]), props);
+		// find first non-equal property
+		var index = R.findIndex(R.equals(false), equalProps);
+		// if found then compare that property
+		if (index >= 0)
+			return R.comparator(propCompares[index])(a[props[index]], b[props[index]]);
+		// return all properties equal
+		return 0;
+	})
+};
