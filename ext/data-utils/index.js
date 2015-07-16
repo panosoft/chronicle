ff599ac08dc6a7b84564223525a16c5abe235375
Charles Scalfani <charles@panosoft.com>
2015-07-16 06:27:41 -0700

    Added utils that are normally done in SQL

--- ext/data-utils/index.js
+++ ext/data-utils/index.js
@@ -2,12 +2,8 @@ var R = require('ramda');
 
 var DU = {
 	callOn: R.curry((obj, f) => f(obj)),
-	sumProps: R.curry((props, obj) => {
-		return R.sum(R.map(DU.callOn(obj), R.map(R.prop, props)));
-	}),
-	sumColumn: R.curry((prop, objs) => {
-		return R.sum(R.map(R.prop(prop), objs));
-	}),
+	sumProps: R.curry((props, obj) => R.sum(R.map(DU.callOn(obj), R.map(R.prop, props)))),
+	sumColumn: R.curry((prop, objs) => R.sum(R.map(R.prop(prop), objs))),
 	compareProps: R.curry(function(props, a, b) {
 		// determine property compare function (lt or gt) based on + or -
 		var propCompares = R.map(prop => prop[0] == '-' ? R.gt : R.lt, props);
@@ -22,7 +18,10 @@ var DU = {
 			return R.comparator(propCompares[index])(a[props[index]], b[props[index]]);
 		// return all properties equal
 		return 0;
-	})
+	}),
+	filterEmpty: R.filter(s => R.trim(s) != ''),
+	fullName: (f, m, l) => R.join(' ', DU.filterEmpty([f, m, l])),
+	cityStateZip: (c, s, z) => c + ', ' + s + ' ' + z
 };
 
 module.exports = DU;
