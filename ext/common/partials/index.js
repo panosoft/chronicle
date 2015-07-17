e64566ab1390953570caa6ffbdd7781e099b7e90
Charles Scalfani <charles@panosoft.com>
2015-07-17 11:47:53 -0700

    Inlined fullName, Added cityStateZip and address partials

--- ext/common/partials/index.js
+++ ext/common/partials/index.js
@@ -5,7 +5,9 @@ var datauri = require('datauri');
 module.exports = {
 	page: '<span style="content: counter(page)"></span>',
 	pages: '<span style="content: counter(pages)"></span>',
-	fullName: fs.readFileSync(path.resolve(__dirname, './fullName.html'), 'utf8'),
+	fullName: '{{f}} {{m}} {{l}}',
+	cityStateZip: '{{c}}, {{s}} {{z}}',
+	address: fs.readFileSync(path.resolve(__dirname, './address.html'), 'utf8'),
 	logo: datauri(path.resolve(__dirname, '../assets/images/panoLogo.png')),
 	micr: fs.readFileSync(path.resolve(__dirname, './micr/main.html'), 'utf8'),
 	micrFont: datauri(path.resolve(__dirname, './micr/PrecisionID MICR.ttf'))
