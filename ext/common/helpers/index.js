cce18ed0925d665981682cb5284ad71066eb5705
Charles Scalfani <charles@panosoft.com>
2015-07-18 10:30:50 -0700

    Added a trim helper that takes MULTIPLE string parameters

--- ext/common/helpers/index.js
+++ ext/common/helpers/index.js
@@ -1,8 +1,9 @@
-var _ = require('lodash');
+var R = require('ramda');
 var format = require('./format');
 
-module.exports = _.assign(format, {
-    isNegative: function (value) {
-        return (value < 0) ? 'negative' : '';
-    }
+module.exports = R.merge(format, {
+    isNegative: value => (value < 0) ? 'negative' : '',
+    trim: function() { // arrow functions don't pass arguments correctly yet #bleedingedge
+		return R.trim(R.join(' ', R.map(R.trim, R.slice(0, arguments.length - 1, arguments))));
+	}
 });
