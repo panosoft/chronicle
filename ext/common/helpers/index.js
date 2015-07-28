f47ae6f80aeb56f26dee8ef2a6dbb95b8f562b20
Charles Scalfani <charles@panosoft.com>
2015-07-27 18:29:58 -0700

    Removed unnecessary trim helper

--- ext/common/helpers/index.js
+++ ext/common/helpers/index.js
@@ -2,8 +2,5 @@ var R = require('ramda');
 var format = require('./format');
 
 module.exports = R.merge(format, {
-    isNegative: value => (value < 0),
-    trim: function() { // arrow functions don't pass arguments correctly yet #bleedingedge
-		return R.trim(R.join(' ', R.map(R.trim, R.slice(0, arguments.length - 1, arguments))));
-	}
+    isNegative: value => (value < 0)
 });
